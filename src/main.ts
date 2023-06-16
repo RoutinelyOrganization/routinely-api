e5523b626d01834d3ac27859f0f2165e1211f5f7
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-06-16 19:45:48 -0300

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle(process.env.npm_package_name)
+    .setVersion(process.env.npm_package_version)
+    .setLicense(
+      process.env.npm_package_license,
+      'https://github.com/RoutinelyOrganization/routinely-api/blob/develop/LICENSE'
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
+  await app.listen(process.env.PORT, () => {
+    console.info(`ONN: http://localhost:${process.env.PORT}/`);
+  });
 }
 bootstrap();
