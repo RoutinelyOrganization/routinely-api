2b513f4f4607b571028aec4476e7c99c275698c6
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-06-18 19:20:07 -0300

    chore(dev): updates user credentials

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 const account = {
   name: 'Ada Lovelace',
   email: '5edfa2692bdacc5e6ee805c626c50cb44cebb065f092d9a1067d89f74dacd326',
-  password: '$2b$04$X.eN36HDOKZ4V.fBGbJiveCvVG9RM6Z5rESSxX7O.c40j7O7chjfi',
+  password: '$2b$04$eJIhkVp9S0deCc75.NAeieZEv2DAlfxsGtg7ml3OpID.Gr1VIRKuG',
 };
 
 async function main() {
@@ -31,7 +31,7 @@ async function main() {
       console.info('\n------SEED------');
       console.info('Seed account credentials:');
       console.info(`email: admin@admin.com`);
-      console.info(`pass: admin`);
+      console.info(`pass: Admin@11`);
       console.info('--------END-------\n');
     })
     .catch(() => {
