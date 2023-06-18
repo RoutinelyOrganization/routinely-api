d1e8fa2fb6ff0ddf99e7fc6a4167d98a66c96951
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-06-18 18:43:09 -0300

    feat: creates createAccountDto to validate data types

---
+++ src/modules/Account/account.dtos.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  Matches,
+  IsEmail,
+  IsStrongPassword,
+  IsBoolean,
+} from 'class-validator';
+
+export class CreateAccountDto {
+  @IsNotEmpty()
+  @Matches(/^[a-zA-ZÀ-ÿ ]+$/)
+  @ApiProperty()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @ApiProperty()
+  email: string;
+
+  @IsNotEmpty()
+  @IsStrongPassword()
+  @ApiProperty()
+  password: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  acceptedTerms: boolean;
+}
