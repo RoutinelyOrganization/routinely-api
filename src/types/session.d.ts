c3b36b53f41d4fd47a0fa579307b96787b78f702
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-06-21 03:11:43 -0300

    chore: creates sessions types

---
+++ src/types/session.d.ts
@@ -0,0 +1,27 @@
+import { Session } from '@prisma/client';
+
+export type ICreateSessionRepositoryExpect = Omit<
+  Session,
+  'id' | 'createdAt' | 'updatedAt'
+>;
+
+export type ICreateSessionRepositoryResponse = Pick<
+  Session,
+  'sessionExpiresIn' | 'accountId' | 'permissions' | 'name'
+>;
+
+export type ICreateSessionServiceExpect = Pick<
+  Session,
+  'accountId' | 'permissions' | 'name'
+> & {
+  remember: boolean;
+};
+
+export type ICreateSessionServiceResponse = Pick<
+  Session,
+  'permissions' | 'name'
+> & {
+  token: string;
+  refreshToken: string;
+  expiresInTimestamp: number;
+};
