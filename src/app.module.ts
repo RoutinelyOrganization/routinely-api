893c4902aadc8a486eda5026d8e3bb7a16fef2a0
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-06-21 04:22:22 -0300

    chore: imports modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
-import { AccountModule } from './modules/Account/account.module';
+import { AccountModule, SessionModule } from './modules';
 
 @Module({
-  imports: [PrismaModule, AccountModule],
+  imports: [PrismaModule, AccountModule, SessionModule],
 })
 export class AppModule {}
