81a9b275173e7e74eb204431435e15ae23a88aef
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-06-21 03:09:57 -0300

    chore: creates access types

--- src/types/account.d.ts
+++ src/types/account.d.ts
@@ -1,8 +1,24 @@
 import { Account, Profile } from '@prisma/client';
 
-export type ICreateAccountExpect = Pick<Account, 'email' | 'password'> &
-  Pick<Profile, 'name'> & { acceptedTerms?: boolean };
+export type ICreateAccountRepositoryExpect = Pick<
+  Account,
+  'email' | 'password'
+> &
+  Pick<Profile, 'name'>;
 
-export type ICreateAccountResponse = {
+export type ICreateAccountServiceResponse = {
   message: string;
 };
+
+// Access
+export type IAccessAccountRepositoryResponse = Pick<
+  Account,
+  'id' | 'email' | 'password' | 'permissions'
+> & {
+  profile: Pick<Profile, 'name'>;
+};
+
+export type IAccessAccountServiceResponse = Pick<
+  IAccessAccountRepositoryResponse,
+  'id' | 'permissions' | 'profile'
+>;
