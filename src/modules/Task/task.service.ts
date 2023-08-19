d78ae9d0940aab786efae0e10aacfce5cd73dc0b
Lucas César <43433070+lucascda@users.noreply.github.com>
2023-08-19 17:14:48 -0300

    feat(Task): add validation to dto

--- src/modules/Task/task.service.ts
+++ src/modules/Task/task.service.ts
@@ -5,13 +5,20 @@ import { TaskRepository } from './task.repository';
 @Injectable()
 export class TaskService {
   constructor(private repository: TaskRepository) {}
+
+  private extractHourString(date: Date): string {
+    const nowDateTime = date.toISOString();
+    const nowDate = nowDateTime.split('T')[0];
+    return nowDate;
+  }
+
   async create(createTaskInput: CreateTaskInput) {
     const date = new Date(createTaskInput.date);
     const now = new Date();
-    const nowDateTime = now.toISOString();
-    const nowDate = nowDateTime.split('T')[0];
+    const nowDate = this.extractHourString(now);
     const hms = createTaskInput.hour;
     const hour = new Date(nowDate + 'T' + hms);
+
     const createdTask = await this.repository.create({
       ...createTaskInput,
       date,
