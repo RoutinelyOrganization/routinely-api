114cfde2b0d5610e9a638da56016dbceedb3fb65
Lucas César <43433070+lucascda@users.noreply.github.com>
2023-10-06 18:11:53 -0300

    feat: add IsEarlierThan validation decorator

--- src/modules/Goal/goal.dto.ts
+++ src/modules/Goal/goal.dto.ts
@@ -1,11 +1,16 @@
 import { GoalPeriodicity, GoalType } from '@prisma/client';
-
+import { IsDateString } from 'class-validator';
+import { IsEarlierDateThan } from 'src/utils/decorators/isEarlierDate';
 export class CreateGoalInput {
   description: string;
   goal: string;
+
+  @IsDateString()
+  @IsEarlierDateThan('end_date')
   start_date: Date;
+  @IsDateString()
   end_date: Date;
-  accountId: string;
   periodicity: GoalPeriodicity;
   type: GoalType;
+  accountId: string;
 }
