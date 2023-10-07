87e4fd7cb73d29cd05c09cef3175be2e18b7d9d5
Lucas César <43433070+lucascda@users.noreply.github.com>
2023-10-07 15:15:33 -0300

    feat: add validation to CreateGoalInput

--- src/modules/Goal/goal.dto.ts
+++ src/modules/Goal/goal.dto.ts
@@ -1,16 +1,25 @@
 import { GoalPeriodicity, GoalType } from '@prisma/client';
-import { IsDateString } from 'class-validator';
+import { IsDateString, IsEnum, MaxLength } from 'class-validator';
 import { IsEarlierDateThan } from 'src/utils/decorators/isEarlierDate';
 export class CreateGoalInput {
+  @MaxLength(1000)
   description: string;
+
+  @MaxLength(200)
   goal: string;
 
   @IsDateString()
   @IsEarlierDateThan('end_date')
   start_date: Date;
+
   @IsDateString()
   end_date: Date;
+
+  @IsEnum(GoalPeriodicity)
   periodicity: GoalPeriodicity;
+
+  @IsEnum(GoalType)
   type: GoalType;
+
   accountId: string;
 }
