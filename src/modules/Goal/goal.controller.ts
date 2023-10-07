084cdcebfaade686558eab92cf21040ec4e93382
Lucas César <43433070+lucascda@users.noreply.github.com>
2023-10-07 15:27:55 -0300

    docs: add CreateGoalInput api properties

--- src/modules/Goal/goal.controller.ts
+++ src/modules/Goal/goal.controller.ts
@@ -4,11 +4,13 @@ import { GoalService } from './goal.service';
 import { Permissions, RequirePermissions, RolesGuard } from 'src/guards';
 import { Request } from 'express';
 import { CREDENTIALS_KEY } from 'src/utils/constants';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('goals')
 export class GoalController {
   constructor(private goalService: GoalService) {}
 
+  @ApiTags('Goals')
   @Post()
   @UseGuards(RolesGuard)
   @RequirePermissions([Permissions['400']])
--- src/modules/Goal/goal.dto.ts
+++ src/modules/Goal/goal.dto.ts
@@ -1,25 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { GoalPeriodicity, GoalType } from '@prisma/client';
 import { IsDateString, IsEnum, MaxLength } from 'class-validator';
 import { IsEarlierDateThan } from 'src/utils/decorators/isEarlierDate';
 export class CreateGoalInput {
+  @ApiProperty({ maxLength: 1000 })
   @MaxLength(1000)
   description: string;
 
+  @ApiProperty({ maxLength: 200 })
   @MaxLength(200)
   goal: string;
 
+  @ApiProperty({ example: '2023-09-14' })
   @IsDateString()
   @IsEarlierDateThan('end_date')
   start_date: Date;
 
+  @ApiProperty({ example: '2023-09-20' })
   @IsDateString()
   end_date: Date;
 
+  @ApiProperty({ enum: GoalPeriodicity, example: 'monthly' })
   @IsEnum(GoalPeriodicity)
   periodicity: GoalPeriodicity;
 
+  @ApiProperty({ enum: GoalType, example: 'personal' })
   @IsEnum(GoalType)
   type: GoalType;
 
+  @ApiProperty()
   accountId: string;
 }
