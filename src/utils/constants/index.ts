61c0d94f3086b540315cf60bece3054fe2a70b7b
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-11-17 17:56:28 -0300

    chore: add development token expires date

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,4 +1,5 @@
 export const CREDENTIALS_KEY = 'account.credentials';
 export const hexadecimalRegex = /^[a-f0-9]+$/;
-export const expirationWhenRememberIsOnn = 36e5 * 24 * 7;
-export const expirationWhenRememberIsOff = 36e5;
+export const expirationWhenRememberIsOff =
+  process.env.NODE_ENV === 'production' ? 36e5 : 3e5;
+export const expirationWhenRememberIsOnn = expirationWhenRememberIsOff * 24 * 7;
