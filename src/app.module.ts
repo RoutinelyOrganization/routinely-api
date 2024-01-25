52042bfffd347b31a05bdf98e84a28778b28a953
Rennanbarao0 <rennanbezerra07@gmail.com>
2024-01-25 20:53:01 -0300

    chore(environment): update variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AccountModule, TaskModule, GoalModule } from './modules';
 
 @Module({
-  imports: [AccountModule, TaskModule, GoalModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AccountModule,
+    TaskModule,
+    GoalModule,
+  ],
 })
 export class AppModule {}
