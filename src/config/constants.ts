0948cef7904287a6752aa3425b58f0ff75d3bd0b
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2024-03-11 14:17:37 -0300

    feat: update constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,6 @@
 export const APP_NAME = 'Routinely API';
 export const APP_VERSION = process.env.npm_package_version;
 export const APP_LICENSE = process.env.npm_package_license;
+
+export const TimezonePtBR = 'GMT-0300';
+export const DateRegex = /^[0-9]{4}(-[0-9]{2}){2} (:?[0-9]{2}){2,3}?$/;
