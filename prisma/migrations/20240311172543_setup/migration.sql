d0e32c9ade864fa6aacb4a4d77df105a8b76f992
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2024-03-12 15:18:01 -0300

    chore: update schema

--- prisma/migrations/20240311172543_setup/migration.sql
+++ prisma/migrations/20240311172543_setup/migration.sql
@@ -8,7 +8,10 @@ CREATE TYPE "GoalType" AS ENUM ('personal', 'profissional');
 CREATE TYPE "TaskPriorities" AS ENUM ('low', 'medium', 'high', 'urgent');
 
 -- CreateEnum
-CREATE TYPE "TaskTags" AS ENUM ('personal', 'study', 'finance', 'career', 'health');
+CREATE TYPE "TaskTags" AS ENUM ('application', 'account', 'exercise', 'beauty', 'literature');
+
+-- CreateEnum
+CREATE TYPE "TaskCategories" AS ENUM ('personal', 'study', 'finance', 'career', 'health');
 
 -- CreateTable
 CREATE TABLE "accounts" (
@@ -18,8 +21,8 @@ CREATE TABLE "accounts" (
     "permissions" TEXT[],
     "verified_at" TIMESTAMP(3),
     "accepted_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL,
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
 );
@@ -29,7 +32,7 @@ CREATE TABLE "ResetPasswordToken" (
     "id" SERIAL NOT NULL,
     "token" TEXT NOT NULL,
     "account_id" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "ResetPasswordToken_pkey" PRIMARY KEY ("id")
 );
@@ -38,8 +41,8 @@ CREATE TABLE "ResetPasswordToken" (
 CREATE TABLE "profiles" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL,
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "account_id" TEXT NOT NULL,
 
     CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
@@ -56,8 +59,8 @@ CREATE TABLE "sessions" (
     "remember" BOOLEAN NOT NULL DEFAULT false,
     "session_expires_in" TIMESTAMP(3) NOT NULL,
     "refresh_expires_in" TIMESTAMP(3) NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL,
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
 );
@@ -66,14 +69,15 @@ CREATE TABLE "sessions" (
 CREATE TABLE "tasks" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "hour" TIME NOT NULL,
-    "date" DATE NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
     "description" TEXT NOT NULL,
     "priority" "TaskPriorities" NOT NULL,
     "tag" "TaskTags" NOT NULL,
+    "category" "TaskCategories" NOT NULL,
+    "checked" BOOLEAN NOT NULL DEFAULT false,
     "account_id" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL,
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
 );
@@ -88,8 +92,8 @@ CREATE TABLE "goals" (
     "periodicity" "GoalPeriodicity" NOT NULL,
     "type" "GoalType" NOT NULL,
     "account_id" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL,
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "goals_pkey" PRIMARY KEY ("id")
 );
