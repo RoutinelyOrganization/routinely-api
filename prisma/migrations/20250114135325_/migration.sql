d8218176ef6ff236410fd4f315fb1dbe4a529add
staviasz <erickesc21@hotmail.com>
2025-01-14 11:05:28 -0300

    fix: correct refresh session

---
+++ prisma/migrations/20250114135325_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `quantityPerWeek` on the `tasks` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "tasks" DROP COLUMN "quantityPerWeek";
